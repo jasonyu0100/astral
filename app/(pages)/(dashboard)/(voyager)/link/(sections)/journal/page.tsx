6db7d23ab703940fed913632ecc8b72f7244a386
Jason <jasonyu0100@gmail.com>
2023-12-23 01:38:34 +0000

    adding journal side bar contents

--- app/(pages)/(dashboard)/(voyager)/link/(sections)/journal/page.tsx
+++ app/(pages)/(dashboard)/(voyager)/link/(sections)/journal/page.tsx
@@ -9,6 +9,7 @@ import StoryRecordLog from "../../(common)/controller/record/log/main";
 import StoryRecord from "../../(common)/controller/record/main";
 import Layer from "@/(pages)/(common)/layer/main";
 import { useState } from "react";
+import StormSidePanelAvatar from "@/(pages)/(dashboard)/(apollo)/storm/(common)/controller/side-panel/avatar/main";
 
 export default function Page() {
   const [navigationActive, changeNavigation] = useState(true);
@@ -19,29 +20,81 @@ export default function Page() {
         containerStyle={containerStyles["row"]}
         backgroundStyle={backgroundStyles["glass-5"]}
       >
-        <div className="flex-grow h-full">
-          <Layer
-            sizeStyle="w-full h-[80px]"
-            backgroundStyle={backgroundStyles["glass-10"]}
-            contentStyle={"px-[4rem] space-x-[1rem]"}
-            containerStyle={containerStyles["row-center"]}
-          ></Layer>
-          <div style={{ height: "calc(100% - 80px)", width: "100%" }}>
-            <StoryController>
-              <StoryRecord>
-                <StoryRecordLog></StoryRecordLog>
-                <StoryRecordLog></StoryRecordLog>
-              </StoryRecord>
-            </StoryController>
-          </div>
-        </div>
+        <StoryController>
+          <StoryRecord>
+            <StoryRecordLog></StoryRecordLog>
+            <StoryRecordLog></StoryRecordLog>
+          </StoryRecord>
+        </StoryController>
         {navigationActive && (
           <Layer
             sizeStyle="w-[350px] h-full"
-            containerStyle={containerStyles["row"]}
+            containerStyle={containerStyles["col"]}
             backgroundStyle={backgroundStyles["glass-5"]}
             borderStyle={borderStyles["border-l"]}
-          ></Layer>
+          >
+            <div className="w-full p-[2rem] flex flex-col space-y-[4rem]">
+              <div className="flex flex-col space-y-[2rem]">
+                <div className="flex flex-row items-center space-x-[1rem]">
+                  <StormSidePanelAvatar />
+                  <p className="text-white font-bold text-lg">
+                    Hello World - Chat
+                  </p>
+                </div>
+              </div>
+              <div className="flex flex-col space-y-[2rem]">
+                <p className="text-slate-500 font-extraBold text-xl">
+                  Yesterday
+                </p>
+                <div className="flex flex-row items-center space-x-[1rem]">
+                  <StormSidePanelAvatar />
+                  <p className="text-white font-bold text-lg">
+                    Hello World - Chat
+                  </p>
+                </div>
+                <div className="flex flex-row items-center space-x-[1rem]">
+                  <StormSidePanelAvatar />
+                  <p className="text-white font-bold text-lg">
+                    Hello World - Chat
+                  </p>
+                </div>
+              </div>
+              <div className="flex flex-col space-y-[2rem]">
+                <p className="text-slate-500 font-extraBold text-xl">
+                  Last Week
+                </p>
+                <div className="flex flex-row items-center space-x-[1rem]">
+                  <StormSidePanelAvatar />
+                  <p className="text-white font-bold text-lg">
+                    Hello World - Chat
+                  </p>
+                </div>
+                <div className="flex flex-row items-center space-x-[1rem]">
+                  <StormSidePanelAvatar />
+                  <p className="text-white font-bold text-lg">
+                    Hello World - Chat
+                  </p>
+                </div>
+              </div>
+              <div className="flex flex-col space-y-[2rem]">
+                <p className="text-slate-500 font-extraBold text-xl">
+                  Last Month
+                </p>
+                <div className="flex flex-row items-center space-x-[1rem]">
+                  <StormSidePanelAvatar />
+                  <p className="text-white font-bold text-lg">
+                    Hello World - Chat
+                  </p>
+                </div>
+                <div className="flex flex-row items-center space-x-[1rem]">
+                  <StormSidePanelAvatar />
+                  <p className="text-white font-bold text-lg">
+                    Hello World - Chat
+                  </p>
+                </div>
+              </div>
+            </div>
+          </Layer>
         )}
       </Layer>
     </>
