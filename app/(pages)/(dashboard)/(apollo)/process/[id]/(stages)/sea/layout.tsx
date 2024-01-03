be82d6da9a8dcd117382ce94730289ef7b9e0dc2
Jason <jasonyu0100@gmail.com>
2024-01-03 01:05:22 +0000

    fixing sea layout bug

--- app/(pages)/(dashboard)/(apollo)/process/[id]/(stages)/sea/layout.tsx
+++ app/(pages)/(dashboard)/(apollo)/process/[id]/(stages)/sea/layout.tsx
@@ -2,7 +2,7 @@ import { DashboardContent } from "@/(pages)/(dashboard)/(common)/content/main";
 import { DashboardBody } from "@/(pages)/(dashboard)/(common)/controller/body/main";
 import { ProcessTabStage, ProcessTabs } from "@/(pages)/(dashboard)/(apollo)/process/[id]/tabs/main";
 
-export function Layout({ children }: { children: React.ReactNode }) {
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <DashboardBody>
       <ProcessTabs tab={ProcessTabStage.Sea} />
