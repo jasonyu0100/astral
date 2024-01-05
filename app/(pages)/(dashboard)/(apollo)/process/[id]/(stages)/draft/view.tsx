cbd32101c3837959437d64c7f2fd88325af7c7a9
Jason Yu <jasonyu0100@gmail.com>
2024-01-05 22:46:19 +1100

    adding experimental modal

--- app/(pages)/(dashboard)/(apollo)/process/[id]/(stages)/draft/view.tsx
+++ app/(pages)/(dashboard)/(apollo)/process/[id]/(stages)/draft/view.tsx
@@ -10,7 +10,7 @@ import { DraftStepsAdd } from "./draft-epic/bottom/add/main";
 import { DraftConstellationSection } from "./draft-epic/center/constellation/main";
 import { ConstellationLinks } from "./draft-epic/center/constellation/stars/links/main";
 import { ConstellationStar } from "./draft-epic/center/constellation/stars/star/main";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { motion } from "framer-motion";
 import { DraftWrapper } from "./draft-epic/wrapper/main";
 import { ProcessStepObj } from "../../model/process/step/main";
@@ -26,6 +26,28 @@ interface DraftViewProps {
   stepHandling: StepHandler;
 }
 
+const Modal = ({ isOpen, onClose }) => {
+  return (
+    <div
+      className={`fixed inset-0 ${
+        isOpen ? "" : "hidden"
+      } flex justify-center items-center z-50`}
+    >
+      <div className="bg-white p-8 shadow-lg w-[650px] h-[770px] z-50">
+        <h2 className="text-2xl font-bold mb-4">Modal Title</h2>
+        <p className="mb-4">Modal content goes here.</p>
+        <button
+          onClick={onClose}
+          className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
+        >
+          Close Modal
+        </button>
+      </div>
+      <div className="fixed inset-0 bg-slate-950 opacity-50 pointer-events-none"></div>
+    </div>
+  );
+};
+
 export function DraftView({
   stepId,
   steps,
@@ -35,11 +57,24 @@ export function DraftView({
 }: DraftViewProps) {
   const constraintsRef = useRef(null);
 
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  const openModal = () => {
+    setModalIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+  };
   return (
     <DraftWrapper>
       <DraftController>
         <DraftControllerCenterSection>
-          <DraftConstellationSection>
+          <button className="text-white" onClick={openModal}>
+            click me
+          </button>
+          <Modal isOpen={modalIsOpen} onClose={closeModal} />
+          {/* <DraftConstellationSection>
             <ConstellationLinks stars={stars} />
             <motion.div
               className="absolute top-0 left- 0 w-full h-full"
@@ -53,7 +88,7 @@ export function DraftView({
                 />
               ))}
             </motion.div>
-          </DraftConstellationSection>
+          </DraftConstellationSection> */}
         </DraftControllerCenterSection>
         <DraftControllerBottomRow>
           <DraftControllerSteps>
@@ -75,7 +110,7 @@ export function DraftView({
           />
         </DraftControllerBottomRow>
       </DraftController>
-      <Sidebar/>
+      <Sidebar />
     </DraftWrapper>
   );
 }
