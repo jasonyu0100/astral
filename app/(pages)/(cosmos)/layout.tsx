a13ca7b20bf30ed83a02cef91088902386529b8f
Jason <jasonyu0100@gmail.com>
2024-01-06 03:12:47 +0000

    adding google auth

--- app/(pages)/(cosmos)/layout.tsx
+++ app/(pages)/(cosmos)/layout.tsx
@@ -1,6 +1,9 @@
 import { CosmosBackground } from "../(common)/background/cosmos/main";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   const isCosmosMode = process.env.NEXT_PUBLIC_COSMOS_MODE === "true";
-  return <CosmosBackground>{isCosmosMode && <>{children}</>}</CosmosBackground>;
+  return (
+      <CosmosBackground>{isCosmosMode && <>{children}</>}</CosmosBackground>
+  );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,17 @@
+"use client";
 import clsx from "clsx";
 import "./globals.css";
 import { fontVariables } from "./fonts";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { GoogleLogin } from "@react-oauth/google";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   const isMaintenanceMode = process.env.NEXT_PUBLIC_MAINTENANCE_MODE === "true";
+  console.log(process.env)
+  const temp = "1030462157669-c6fsghjhc8pbrqotqhgm7elj5k9mbdof.apps.googleusercontent.com" 
+
+  const responseMessage = () => {};
+  const errorMessage = () => {};
 
   return (
     <html lang="en">
@@ -11,7 +19,15 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         <meta name="referrer" content="origin" />
       </head>
       <body className={clsx([...fontVariables])}>
-        {!isMaintenanceMode && <>{children}</>}
+        <GoogleOAuthProvider
+          clientId={temp || ""}
+        >
+          {!isMaintenanceMode && (
+            <>
+              <GoogleLogin onSuccess={responseMessage} onError={errorMessage} />
+            </>
+          )}
+        </GoogleOAuthProvider>
       </body>
     </html>
   );
