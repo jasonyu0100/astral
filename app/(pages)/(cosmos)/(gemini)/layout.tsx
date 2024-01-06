c06e3037e4b24a41df207ae769e0c3d93eb1508f
Jason <jasonyu0100@gmail.com>
2024-01-06 02:39:15 +0000

    fixing build error

--- app/(pages)/(cosmos)/(gemini)/layout.tsx
+++ app/(pages)/(cosmos)/(gemini)/layout.tsx
@@ -1,17 +1,17 @@
-import { Background } from "@/(pages)/(common)/background/main";
 import { Topbar } from "../../(common)/topbar/main";
 import { DashboardController } from "../(common)/controller/main";
 import { DashboardSidebar } from "../(common)/sidebar/main";
 import { DashboardBody } from "../(common)/controller/body/main";
+import { CosmosBackground } from "@/(pages)/(common)/background/cosmos/main";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <Background>
+    <CosmosBackground>
       <Topbar />
       <DashboardController>
         <DashboardSidebar />
         <DashboardBody>{children}</DashboardBody>
       </DashboardController>
-    </Background>
+    </CosmosBackground>
   );
 }
