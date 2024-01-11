f9b9400e56380d79a91830764d02ab1d71e3c9f5
Jason Yu <jasonyu0100@gmail.com>
2024-01-11 19:04:12 +1100

    fixing auth

--- app/utils/isAuth.tsx
+++ app/utils/isAuth.tsx
@@ -1,17 +1,17 @@
 "use client";
 import { useEffect } from "react";
 import { redirect } from "next/navigation";
+import { useUser } from "@/state/main";
 
 export default function isAuth(Component: any) {
   return function IsAuth(props: any) {
-    const auth = localStorage?.user?.googleId != null;
+    const auth = JSON.parse(localStorage?.user || {}).googleId != undefined;
 
     useEffect(() => {
       if (!auth) {
         return redirect("/");
       }
     }, []);
-
 
     if (!auth) {
       return null;
