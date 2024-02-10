54f9690641774cfddd1dd1e4eacc0f7049dddd29
Jason <jasonyu0100@gmail.com>
2024-02-10 04:15:27 +0000

    adding run code gen step

--- run_codegen.sh
+++ run_codegen.sh
@@ -1,12 +1,37 @@
 #!/bin/bash
 
-# Run amplify codegen
-amplify codegen
+echo "AWS Credential Configuration Script"
 
-# Check if the codegen process was successful
+# Specify the path to the .env.local file
+ENV_FILE=".env.local"
+
+# Check if the .env.local file exists
+if [ -f "$ENV_FILE" ]; then
+    # Export variables from .env.local
+    export $(grep -v '^#' "$ENV_FILE" | xargs -0)
+
+    # Validate if AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY are set
+    if [ -z "$AWS_ACCESS_KEY_ID" ] || [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
+        echo "Error: AWS access key ID and secret access key cannot be empty."
+        exit 1
+    fi
+
+    # Write credentials to ~/.aws/credentials file
+    echo "[default]" > ~/.aws/credentials
+    echo "aws_access_key_id = $AWS_ACCESS_KEY_ID" >> ~/.aws/credentials
+    echo "aws_secret_access_key = $AWS_SECRET_ACCESS_KEY" >> ~/.aws/credentials
+
+    echo "AWS credentials configured successfully in ~/.aws/credentials"
+else
+    echo "Error: $ENV_FILE not found. Please make sure the file exists."
+fi
+
+
+npm install -g @aws-amplify/cli
+npx @aws-amplify/cli codegen
+
 if [ $? -eq 0 ]; then
-    # Copy the folder server/graphql to app/graphql
-    cp -r server/graphql app/
+    cp -r app/graphql server/
     echo "Code generation and folder copy completed successfully."
 else
     echo "Error: Code generation failed. Please check the logs for more information."
