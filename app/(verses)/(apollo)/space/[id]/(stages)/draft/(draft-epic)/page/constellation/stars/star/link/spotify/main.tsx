b8c69781a7a5eb3e2b6188e59d281718e3c2b7aa
Jason <jasonyu0100@gmail.com>
2024-02-17 05:38:02 +0000

    adding youtube and spotify basic embeds

--- app/(verses)/(apollo)/space/[id]/(stages)/draft/(draft-epic)/page/constellation/stars/star/link/spotify/main.tsx
+++ app/(verses)/(apollo)/space/[id]/(stages)/draft/(draft-epic)/page/constellation/stars/star/link/spotify/main.tsx
@@ -4,7 +4,8 @@ import { StarMotionContext } from '@/(verses)/(handler)/draft/star/main';
 import { DraftContext } from '@/(verses)/(apollo)/space/[id]/(stages)/draft/page';
 
 export function LinkSpotifyStar() {
-  const { star, x,y, constraintsRef, activateStar} = useContext(StarMotionContext);
+  const { star, x, y, constraintsRef, activateStar } =
+    useContext(StarMotionContext);
   const { starId } = useContext(DraftContext);
   const active = starId === star.id;
 
@@ -13,19 +14,30 @@ export function LinkSpotifyStar() {
       <motion.div
         drag
         dragConstraints={constraintsRef}
-        className={`absolute flex h-[150px] w-[100px] top-[${x}] left-[${y}]`}
+        className={`absolute flex aspect-[16/12] w-[300px] top-[${x}] left-[${y}] bg-red-500`}
         style={{ x, y }}
       >
         <div
-          className='flex h-full w-full flex-shrink-0 flex-col'
+          className='flex aspect-[16/9] h-full w-full flex-shrink-0 flex-col'
           onDragStart={() => {
             activateStar();
           }}
         >
-          <img
-            className='pointer-events-none aspect-square h-[100px] w-[100px] flex-shrink-0 rounded-full border-[2px] border-slate-300'
-            src={star?.file?.src}
-          />
+          <iframe
+            src='https://open.spotify.com/embed/track/14I47nVJiJt9NCzt7YmnWz?utm_source=generator'
+            width='100%'
+            height='100%'
+            allow='autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture'
+            loading='lazy'
+          ></iframe>
+          {/* <iframe
+            width='560'
+            height='315'
+            src={star.link.url}
+            title='YouTube video player'
+            allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+            allowfullscreen
+          ></iframe> */}
           <div className='flex h-[50px] items-center'>
             <p className='w-full text-center font-bold text-slate-300'>
               {star.title} {star.variant} {star.file?.variant}
@@ -36,5 +48,3 @@ export function LinkSpotifyStar() {
     </>
   );
 }
-
-
--- app/(verses)/(modals)/star-modal/add-link/main.tsx
+++ app/(verses)/(modals)/star-modal/add-link/main.tsx
@@ -4,31 +4,50 @@ import { FormFooter } from '@/(common)/form/footer/main';
 import { FormContainer } from '@/(common)/form/main';
 import { FormTitle } from '@/(common)/form/title/main';
 import { Modal } from '@/(common)/modal/main';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { StarModalContext } from '../main';
 import { DraftContext } from '@/(verses)/(apollo)/space/[id]/(stages)/draft/page';
 import { LinkObj, LinkVariant } from '@/(ouros)/(model)/resource/link/main';
+import { FormSelect } from '@/(common)/form/select/main';
 
 export function AddLinkStarModal() {
   const modalContext = useContext(StarModalContext);
   const { opened, close } = modalContext.addLinkStarModal;
   const { starHandler } = useContext(DraftContext);
+  const [variant, changeVariant] = useState(LinkVariant.YOUTUBE);
 
   return (
     <Modal isOpen={opened} onClose={() => close()}>
       <FormContainer>
         <FormTitle>Link Element</FormTitle>
         <FormBody>
+          <FormSelect
+            title='Variant'
+            value={variant}
+            onChange={(e) => changeVariant(e.target.value)}
+          >
+            <option value={LinkVariant.YOUTUBE}>YouTube</option>
+            <option value={LinkVariant.SPOTIFY}>Spotify</option>
+          </FormSelect>
         </FormBody>
         <FormFooter>
           <FormButton
             onClick={() => {
-              starHandler.queryCreateLinkStar("hello", 0, 0, {
-                id: '0',
-                title: 'passion.png',
-                url: 'https://www.youtube.com/watch?v=AcsCP1C_Zd8',
-                variant: LinkVariant.YOUTUBE,
-              } as LinkObj);
+              if (variant === LinkVariant.YOUTUBE) {
+                starHandler.queryCreateLinkStar('hello', 0, 0, {
+                  id: '0',
+                  title: 'passion.png',
+                  url: 'https://www.youtube.com/embed/A6XUVjK9W4o',
+                  variant: LinkVariant.YOUTUBE,
+                } as LinkObj);
+              } else if (variant === LinkVariant.SPOTIFY) {
+                starHandler.queryCreateLinkStar('hello', 0, 0, {
+                  id: '0',
+                  title: 'passion.png',
+                  url: 'https://open.spotify.com/embed/track/14I47nVJiJt9NCzt7YmnWz?utm_source=generator',
+                  variant: LinkVariant.SPOTIFY,
+                } as LinkObj);
+              }
               close();
             }}
           >
@@ -39,4 +58,3 @@ export function AddLinkStarModal() {
     </Modal>
   );
 }
-
