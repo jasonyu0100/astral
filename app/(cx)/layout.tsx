170f09319d60175a990d1aa9afd05ba311dedf4c
Jason <jasonyu0100@gmail.com>
2024-04-10 03:28:17 +0000

    FIXING BUILDS

--- app/(cx)/layout.tsx
+++ app/(cx)/layout.tsx
@@ -1,4 +1,4 @@
-import { VerseBackground } from '../(components)/(app)/background/verse/main';
+import { VerseBackground } from "@/(components)/(app)/background/verses/main";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return <VerseBackground>
