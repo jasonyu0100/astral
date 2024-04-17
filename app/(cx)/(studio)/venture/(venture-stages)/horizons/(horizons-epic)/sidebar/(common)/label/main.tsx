d91fe2b6ecd174c4d50bbfb86def1b6ce3de3151
Jason Yu <jasonyu0100@gmail.com>
2024-04-17 10:32:29 +1000

    Seek Horizons

--- app/(cx)/(studio)/venture/(venture-stages)/horizons/(horizons-epic)/sidebar/(common)/label/main.tsx
+++ app/(cx)/(studio)/venture/(venture-stages)/horizons/(horizons-epic)/sidebar/(common)/label/main.tsx
@@ -4,7 +4,7 @@ export function HorizonsSidebarLabel({
   children: React.ReactNode;
 }) {
   return (
-    <div className='flex h-[4rem] flex-shrink-0 items-center'>
+    <div className='flex h-[2rem] mb-[1rem] flex-shrink-0 items-center'>
       <p className='font-extraBold text-lg text-slate-500'>{children}</p>
     </div>
   );
--- app/(cx)/(studio)/venture/(venture-stages)/horizons/(horizons-epic)/sidebar/main.tsx
+++ app/(cx)/(studio)/venture/(venture-stages)/horizons/(horizons-epic)/sidebar/main.tsx
@@ -8,18 +8,18 @@ export function HorizonsSidebar() {
   return (
     <GlassAreaContainer
       name={HorizonsSidebar.name}
-      sizeFx='max-w-[400px] min-w-[250px] w-1/4 h-full p-[1rem]'
+      sizeFx='max-w-[400px] min-w-[250px] w-1/4 h-full'
       borderFx={borderFx['border-l']}
       glassFx={glassFx['glass-5']}
-      className='flex flex-col space-y-[2rem] overflow-auto p-[1rem]'
+      className='flex flex-col p-[1rem]'
     >
-    <div className='flex h-full w-full flex-col overflow-auto pr-[1rem]'>
-      <div className='flex w-full flex-col space-y-[1rem]'>
+      <div className='flex h-full w-full flex-col overflow-auto pr-[1rem]'>
+        <div className='flex w-full flex-col space-y-[1rem]'>
+          <HorizonsSidebarScale />
+          <HorizonsSidebarModal />
+          <HorizonsSidebarDegree />
+        </div>
       </div>
-    </div>
-      <HorizonsSidebarScale />
-      <HorizonsSidebarModal />
-      <HorizonsSidebarDegree />
     </GlassAreaContainer>
   );
 }
