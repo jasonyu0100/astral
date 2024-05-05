34435e221c115b9ec148ff5dcb334697bf216094
Jason <jasonyu0100@gmail.com>
2024-05-05 14:03:15 +0000

    more nuance

--- app/(mx)/(support)/(game)/dilemma/page.tsx
+++ app/(mx)/(support)/(game)/dilemma/page.tsx
@@ -104,7 +104,7 @@ export default function Page() {
           </p>
           <p className='text-md font-bold text-slate-500'>
             Dual is a 1v1 game where players place cards in a 2x2 grid to score
-            points for your side
+            points for your side or yourself
           </p>
           <p className='text-md font-bold text-slate-500'>
             Dilemma is a 2v2 game where players place cards in a 2x2 grid to
