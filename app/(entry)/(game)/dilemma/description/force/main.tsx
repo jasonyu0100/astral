3592f128d52a98ab3f952fe048116b0de9f74056
Jason <jasonyu0100@gmail.com>
2024-05-10 09:18:19 +0000

    authorised dual

--- app/(entry)/(game)/dilemma/description/force/main.tsx
+++ app/(entry)/(game)/dilemma/description/force/main.tsx
@@ -1,18 +1,35 @@
+/* eslint-disable react/no-unescaped-entities */
+
+import { useState } from 'react';
+
 export function Force() {
-    return <div className='flex flex-col'>
-      <p className='text-lg font-bold text-slate-300'>Force</p>
-      <br />
-      <p className='text-md font-bold text-slate-500'>
-        Force - Conditional modifier that unlocks fusion/fission
-      </p>
-      <p className='text-md font-bold text-slate-500'>
-        Fusion - Conditional modifier that lets dealer multiply
-      </p>
-      <p className='text-md font-bold text-slate-500'>
-        Fission - Conditional modifier that lets dealer divide
-      </p>
-      <p>
-        Dualist - Exceptional dealer that can multiply and divide
-      </p>
+  const [authorised, setAuthorised] = useState(false);
+  const [dual, setDual] = useState(false);
+
+  return (
+    <div className='flex flex-col'>
+      {authorised && (
+        <>
+          <p className='text-lg font-bold text-slate-300'>Force</p>
+          <br />
+          <p className='text-md font-bold text-slate-500'>
+            Force - Conditional modifier that unlocks fusion/fission
+          </p>
+          <p className='text-md font-bold text-slate-500'>
+            Fusion - Conditional modifier that lets dealer multiply
+          </p>
+          <p className='text-md font-bold text-slate-500'>
+            Fission - Conditional modifier that lets dealer divide
+          </p>
+          <br />
+          {dual && (
+            <>
+              <p className='hidden'>Dualist - refined player</p>
+              <p className='hidden'>Duelist - undefined player</p>
+            </>
+          )}
+        </>
+      )}
     </div>
+  );
 }
