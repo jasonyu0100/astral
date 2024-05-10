037d50e15eaef2ac906de2a79c98bb4286aa9e22
Jason <jasonyu0100@gmail.com>
2024-05-10 08:16:07 +0000

    ++

--- app/(entry)/(game)/dilemma/description/force/main.tsx
+++ app/(entry)/(game)/dilemma/description/force/main.tsx
@@ -3,13 +3,16 @@ export function Force() {
       <p className='text-lg font-bold text-slate-300'>Force</p>
       <br />
       <p className='text-md font-bold text-slate-500'>
-        Force - Conditional modifier that unlocks dual and fusion/fission
+        Force - Conditional modifier that unlocks fusion/fission
       </p>
       <p className='text-md font-bold text-slate-500'>
         Fusion - Conditional modifier that lets dealer multiply
       </p>
       <p className='text-md font-bold text-slate-500'>
         Fission - Conditional modifier that lets dealer divide
+      </p>
+      <p>
+        Dualist - Exceptional dealer that can multiply and divide
       </p>
     </div>
 }
