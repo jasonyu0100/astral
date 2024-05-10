5169fdf0e2b83c0d10564e75b2e7fd71054d8c86
Jason <jasonyu0100@gmail.com>
2024-05-10 06:34:51 +0000

    adding no mercy clause just in case

--- app/(entry)/(game)/dilemma/description/main.tsx
+++ app/(entry)/(game)/dilemma/description/main.tsx
@@ -92,6 +92,9 @@ export function GameDescription() {
         Hero - Positional modifier that adds onto AB multiplier payouts
       </p>
       <p className='text-md font-bold text-slate-500'>
+        No Mercy - Conditional modifier that makes the hero a multiplier
+      </p>
+      <p className='text-md font-bold text-slate-500'>
         Destination - Region with max QV
       </p>
       <p className='text-md font-bold text-slate-500'>
