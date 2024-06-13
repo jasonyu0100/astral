6bec4148f74bf5c4effb13c1688816d55e807ef3
Jason <jasonyu0100@gmail.com>
2024-06-13 07:31:07 +0000

    conversation start time

--- app/(core)/(project)/space/[id]/(pages)/storm/view/main/core/chat/main.tsx
+++ app/(core)/(project)/space/[id]/(pages)/storm/view/main/core/chat/main.tsx
@@ -2,9 +2,12 @@ import { useContext } from 'react';
 import { StormMessage } from './message/main';
 import { ContextForConversationMessageObj } from '@/(server)/(model)/space/chapter/chat/conversation/message/main';
 import { ContextForConversationMessageList } from '@/(server)/(controller)/space/chapter/chat/conversation/message/list';
+import { HorizontalDivider } from '@/(components)/(line)/divider/horizontal/main';
 
 export function StormChat() {
-  const messageListController = useContext(ContextForConversationMessageList)
+  const conversationListController = useContext(ContextForConversationMessageList);
+  const messageListController = useContext(ContextForConversationMessageList);
+  const conversationObj = conversationListController.state.currentObj;
   
   return (
     <div
@@ -12,6 +15,11 @@ export function StormChat() {
       style={{ height: '100%' }}
     >
       <div className='flex w-full max-w-[900px] flex-col space-y-[1rem] py-[2rem]'>
+        {conversationObj &&
+          <div className="w-full items-center flex flex-col space-y-[1rem]">
+            <p className="text-md text-slate-500 font-bold">{new Date(conversationObj.created).toDateString()}, {new Date(conversationObj.created).toLocaleTimeString()}</p>
+          </div>
+        }
         {messageListController.state.objs.map((message) => (
           <ContextForConversationMessageObj.Provider value={message} key={message.id}>
             <StormMessage key={message.id}/>
