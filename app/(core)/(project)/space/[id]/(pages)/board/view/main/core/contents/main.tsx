5edb834e3604695f9d9f64f1732fd568d2733569
Jason <jasonyu0100@gmail.com>
2024-06-14 14:18:30 +0000

    aligning the visions for verses

--- app/(core)/(project)/space/[id]/(pages)/board/view/main/core/contents/main.tsx
+++ app/(core)/(project)/space/[id]/(pages)/board/view/main/core/contents/main.tsx
@@ -3,9 +3,16 @@ import { SpaceBoardScene } from './scene/main';
 
 export function SpaceBoardCoreContents() {
   return (
-    <div style={{ width: '100%', height: '100%' }} className='flex flex-row'>
-      <SpaceBoardSceneOverlay />
-      <SpaceBoardScene />
+    <div
+      style={{ width: '100%', height: '100%' }}
+      className='relative flex flex-row'
+    >
+      <div style={{ width: '100%', height: '100%' }} className='absolute'>
+        <SpaceBoardSceneOverlay />
+      </div>
+      <div style={{ width: '100%', height: '100%' }} className='absolute'>
+        <SpaceBoardScene />
+      </div>
     </div>
   );
 }
--- app/(core)/(project)/space/[id]/(pages)/verse/view/main/core/contents/main.tsx
+++ app/(core)/(project)/space/[id]/(pages)/verse/view/main/core/contents/main.tsx
@@ -3,9 +3,22 @@ import { SpaceVerseListOverlay } from './overlay/main';
 
 export function SpaceVerseCoreContents() {
   return (
-    <div style={{ width: '100%', height: '100%' }} className='flex flex-row'>
-      <SpaceVerseListOverlay />
-      <SpaceVerseList />
+    <div
+      style={{ width: '100%', height: '100%' }}
+      className='relative flex flex-row'
+    >
+      <div
+        style={{ width: '100%', height: '100%' }}
+        className='absolute flex flex-row'
+      >
+        <SpaceVerseListOverlay />
+      </div>
+      <div
+        style={{ width: '100%', height: '100%' }}
+        className='absolute flex flex-row'
+      >
+        <SpaceVerseList />
+      </div>
     </div>
   );
 }
