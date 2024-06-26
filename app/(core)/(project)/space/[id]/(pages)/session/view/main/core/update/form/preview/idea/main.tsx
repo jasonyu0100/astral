88477460cfbdc7167b2a5168b524bd79b7df95a4
Jason <jasonyu0100@gmail.com>
2024-06-26 10:22:11 +0000

    showing idea preview

--- app/(core)/(project)/space/[id]/(pages)/session/view/main/core/update/form/preview/idea/main.tsx
+++ app/(core)/(project)/space/[id]/(pages)/session/view/main/core/update/form/preview/idea/main.tsx
@@ -1,4 +1,5 @@
 import { useControllerForSceneIdeaMain } from '@/(server)/(controller)/space/chapter/scene/idea/main';
+import { ElementVariant } from '@/(server)/(model)/elements/main';
 import { ContextForChapterSessionUpdateObj } from '@/(server)/(model)/space/chapter/session/update/main';
 import { useContext } from 'react';
 
@@ -11,7 +12,15 @@ export function SpaceSessionUpdatePreviewIdea() {
   return (
     <>
       <p className='text-xl font-bold text-slate-300'>
+        {ideaController.state.obj.variant === ElementVariant.FILE && (
+          <img
+            className='aspect-square h-[200px]'
+            src={ideaController.state.obj.fileElem?.src}
+          />
+        )}
+        <br />
         {ideaController.state.obj.title}
+        {ideaController.state.obj.description}
       </p>
     </>
   );
