7aacff12f0b43a9bff4648b78466d08bc863f109
Jason Yu <jasonyu@jasonyu.lan>
2024-08-21 21:23:32 +1000

    updating the idea

--- app/(core)/(project)/space/[id]/(pages)/map/view/main/core/header/left/main.tsx
+++ app/(core)/(project)/space/[id]/(pages)/map/view/main/core/header/left/main.tsx
@@ -10,7 +10,43 @@ export function SpaceMapHeaderLeft() {
         <SpaceMapCursorIcon />
         <SpaceMapAddIcon />
         <SpaceMapTextIcon />
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          height='24px'
+          viewBox='0 -960 960 960'
+          width='24px'
+          className='fill-slate-300'
+        >
+          <path d='m160-800 80 160h120l-80-160h80l80 160h120l-80-160h80l80 160h120l-80-160h120q33 0 56.5 23.5T880-720v480q0 33-23.5 56.5T800-160H160q-33 0-56.5-23.5T80-240v-480q0-33 23.5-56.5T160-800Zm0 240v320h640v-320H160Zm0 0v320-320Z' />
+        </svg>
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          height='24px'
+          viewBox='0 -960 960 960'
+          width='24px'
+          className='fill-slate-300'
+        >
+          <path d='M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Zm0-80h560v-560H200v560Zm40-80h480L570-480 450-320l-90-120-120 160Zm-40 80v-560 560Z' />
+        </svg>
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          height='24px'
+          viewBox='0 -960 960 960'
+          width='24px'
+          className='fill-slate-300'
+        >
+          <path d='M480-400q-50 0-85-35t-35-85v-240q0-50 35-85t85-35q50 0 85 35t35 85v240q0 50-35 85t-85 35Zm0-240Zm-40 520v-123q-104-14-172-93t-68-184h80q0 83 58.5 141.5T480-320q83 0 141.5-58.5T680-520h80q0 105-68 184t-172 93v123h-80Zm40-360q17 0 28.5-11.5T520-520v-240q0-17-11.5-28.5T480-800q-17 0-28.5 11.5T440-760v240q0 17 11.5 28.5T480-480Z' />
+        </svg>
         <SpaceMapLinkIcon />
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          height='24px'
+          viewBox='0 -960 960 960'
+          width='24px'
+          className='fill-slate-300'
+        >
+          <path d='m422-232 207-248H469l29-227-185 267h139l-30 208ZM320-80l40-280H160l360-520h80l-40 320h240L400-80h-80Zm151-390Z' />
+        </svg>
         {/* <SpaceMapCollaboratorIcon /> */}
         {/* <SpaceMapGroupIcon /> */}
         {/* <SpaceMapFolderIcon /> */}
