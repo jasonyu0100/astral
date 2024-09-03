55b3abf421742a42b5de5f96afe00b65b8477d5d
Jason Yu <jasonyu0100@gmail.com>
2024-09-03 11:54:05 +1000

    refactoring journal

--- app/(core)/(dashboard)/journal/(pages)/record/view/main/core/feed/main.tsx
+++ app/(core)/(dashboard)/journal/(pages)/record/view/main/core/feed/main.tsx
@@ -1,50 +1,74 @@
 import { ContextForCollectionResourceList } from '@/(server)/controller/gallery/collection/resource/list';
 import { FileElemVariant } from '@/(server)/model/elements/file/main';
 import { ElementVariant } from '@/(server)/model/elements/main';
-import { borderFx, glassFx, roundedFx } from '@/style/data';
+import { borderFx, roundedFx } from '@/style/data';
 import { GlassWindowContents } from '@/ui/glass/window/contents/main';
 import { GlassWindowFrame } from '@/ui/glass/window/main';
-import { GlassWindowPane } from '@/ui/glass/window/pane/main';
+import { getFormattedDate } from '@/utils/dateFormat';
 import { useContext } from 'react';
 
 export function JournalRecordFeed() {
   const resourceListController = useContext(ContextForCollectionResourceList);
 
   return (
-    <div className='flex w-full flex-row flex-wrap items-center justify-center gap-[2rem]'>
-      {resourceListController.state.objs.length === 0 && (
-        <p className='text-lg text-slate-500'>No Entries</p>
-      )}
-      {resourceListController.state.objs.map((resource, index) => (
-        <div className='flex flex-col items-center space-y-[1rem]'>
-          <GlassWindowFrame
-            borderFx={borderFx['border-around']}
-            roundedFx={roundedFx.rounded}
-          >
-            <GlassWindowContents className='flex flex-col items-center'>
-              {resource.variant === ElementVariant.FILE && (
-                <>
-                  {resource.fileElem?.variant === FileElemVariant.IMAGE && (
+    <div
+      style={{ height: '100%', width: '100%' }}
+      className='overflow-auto p-[2rem]'
+    >
+      <GlassWindowFrame borderFx={borderFx['border-b']} className='w-full'>
+        <div className='grid w-full grid-cols-7 justify-items-center gap-[3rem] py-[1rem]'>
+          <div className='font-bold text-slate-500'>#</div>
+          <div className='col-span-3 font-bold text-slate-500 '>summary</div>
+          <div className='font-bold text-slate-500'>date</div>
+          <div className='col-span-2 font-bold text-slate-500'>entry</div>
+        </div>
+      </GlassWindowFrame>
+      <div className='flex w-full flex-col'>
+        {resourceListController.state.objs.map((resource, index) => (
+          <div className='grid  w-full grid-cols-7 flex-row items-center justify-items-center gap-[3rem] space-y-[3rem]'>
+            <div>
+              <div className='flex h-[3rem] w-[3rem] items-center justify-center rounded-full bg-blue-500'>
+                <p className='text-lg font-bold text-slate-300'>{index + 1}</p>
+              </div>
+            </div>
+            <div className='col-span-3 p-[1rem]'>
+              <GlassWindowFrame
+                className='w-full p-[1rem]'
+                roundedFx={roundedFx.rounded}
+              >
+                <GlassWindowContents className='h-full w-full p-[1rem] text-center font-light text-slate-300'>
+                  lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Quisquam quod, quae, quos, quibusdam quia quidem quae, quos,
+                  quibusdam quia quidem quae, quos, quibusdam quia quidem quae,
+                  quos, quibusdam quia
+                </GlassWindowContents>
+              </GlassWindowFrame>
+            </div>
+            <div className='text-center text-lg font-bold text-slate-500'>
+              {getFormattedDate(new Date(resource.created))}
+            </div>
+            <div className='col-span-2 flex flex-row justify-items-start'>
+              <GlassWindowFrame className='h-full'>
+                <GlassWindowContents className='flex flex-col'>
+                  {resource.variant === ElementVariant.FILE && (
                     <>
-                      <img
-                        className='h-[200px] object-contain'
-                        src={resource.fileElem.src}
-                        alt={resource.title}
-                      />
+                      {resource.fileElem?.variant === FileElemVariant.IMAGE && (
+                        <>
+                          <img
+                            className='h-[200px] object-contain'
+                            src={resource.fileElem.src}
+                            alt={resource.title}
+                          />
+                        </>
+                      )}
                     </>
                   )}
-                </>
-              )}
-            </GlassWindowContents>
-            <GlassWindowPane glassFx={glassFx['glass-5']} />
-          </GlassWindowFrame>
-          <div className='flex flex-row items-center space-x-[1rem]'>
-            <div className='flex h-[2rem] w-[2rem] items-center justify-center rounded-full bg-blue-500 font-bold text-white'>
-              {index + 1}
+                </GlassWindowContents>
+              </GlassWindowFrame>
             </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 }
--- app/(core)/(dashboard)/journal/(pages)/record/view/main/core/main.tsx
+++ app/(core)/(dashboard)/journal/(pages)/record/view/main/core/main.tsx
@@ -3,11 +3,9 @@ import { JournalRecordHeader } from './header/main';
 
 export function JournalRecordCore() {
   return (
-    <div className='h-full flex-grow overflow-auto'>
-      <div className='flex flex-grow flex-col items-center space-y-[2rem]'>
-        <JournalRecordHeader />
-        <JournalRecordFeed />
-      </div>
+    <div className='flex h-full flex-grow flex-col overflow-auto'>
+      <JournalRecordHeader />
+      <JournalRecordFeed />
     </div>
   );
 }
