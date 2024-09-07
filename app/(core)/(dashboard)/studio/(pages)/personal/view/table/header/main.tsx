292ed4444a526cd8b1176c75d1b218fca2c512ce
Jason Yu <jasonyu0100@gmail.com>
2024-09-07 21:34:23 +1000

    emojification

--- app/(core)/(dashboard)/studio/(pages)/personal/view/table/header/main.tsx
+++ app/(core)/(dashboard)/studio/(pages)/personal/view/table/header/main.tsx
@@ -6,17 +6,19 @@ export function StudioSpacesTableHeader() {
     <div className='w-full px-[3rem]'>
       <GlassWindowFrame borderFx={borderFx['border-b']} className='w-full'>
         <div className='grid w-full grid-cols-7 items-center py-[1rem]'>
-          <p className='text-center font-bold text-slate-300'>#</p>
-          <p className='px-[0.5rem] font-bold text-slate-300'>Space</p>
-          <p className='text-center font-bold text-slate-300'></p>
+          <p className='text-center font-bold text-slate-500'>#</p>
+          <div />
+          <div />
           <div />
           <div className='col-span-2 grid grid-cols-4'>
-            <p className='text-center font-bold text-slate-300'>Day</p>
-            <p className='text-center font-bold text-slate-300'>Ideas</p>
-            <p className='text-center font-bold text-slate-300'>Logs</p>
-            <p className='text-center font-bold text-slate-300'>Spotlights</p>
+            <p className='text-center text-lg font-bold text-slate-300'>🗓️</p>
+            <p className='text-center text-lg font-bold text-slate-300'>💡</p>
+            <p className='text-center text-lg font-bold text-slate-300'>🟨</p>
+            <p className='text-center text-lg font-bold text-slate-300'>🎤</p>
           </div>
-          <div />
+          <div>
+            <p className='text-center text-lg font-bold text-slate-300'>🚀</p>
+          </div>
         </div>
       </GlassWindowFrame>
     </div>
