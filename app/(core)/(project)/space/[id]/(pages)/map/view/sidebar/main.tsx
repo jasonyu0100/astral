d92d3dc7669d0f2a900c275703837894cf6a9a13
Jason Yu <jasonyu0100@gmail.com>
2024-09-07 13:34:22 +1000

    addding sidebar chat

--- app/(core)/(project)/space/[id]/(pages)/map/view/sidebar/main.tsx
+++ app/(core)/(project)/space/[id]/(pages)/map/view/sidebar/main.tsx
@@ -1,3 +1,9 @@
+import { ContextForSceneConversationList } from '@/(server)/controller/space/chapter/scene/conversation/list';
+import { ContextForConversationMessageList } from '@/(server)/controller/space/chapter/scene/conversation/message/list';
+import { glassFx, roundedFx } from '@/style/data';
+import { GlassWindowContents } from '@/ui/glass/window/contents/main';
+import { GlassWindowFrame } from '@/ui/glass/window/main';
+import { GlassWindowPane } from '@/ui/glass/window/pane/main';
 import { useContext } from 'react';
 import {
   ContextForSpaceMap,
@@ -13,6 +19,10 @@ import { SpaceMapSidebarHomeGallerysMode } from './media-explorer/(home)/main';
 import { SpaceMapCollectionResourcesMode } from './media-explorer/(resources)/main';
 
 export function SpaceMapSidebar() {
+  const conversationListController = useContext(
+    ContextForSceneConversationList,
+  );
+  const messageListController = useContext(ContextForConversationMessageList);
   const {
     state: {
       sidebarContentMode: sidebarMode,
@@ -47,7 +57,36 @@ export function SpaceMapSidebar() {
         </>
       )}
 
-      {sidebarMode === SpaceMapSidebarContentMode.CHAT && <></>}
+      {sidebarMode === SpaceMapSidebarContentMode.CHAT && (
+        <div className='flex w-full flex-col' style={{ height: '100%' }}>
+          <GlassWindowFrame className='h-[4rem] w-full flex-shrink-0 shadow-glow'></GlassWindowFrame>
+          <GlassWindowFrame className='h-full w-full flex-shrink-0'>
+            <GlassWindowContents className='flex h-full w-full flex-col'>
+              <div
+                className='flex w-full flex-col space-y-[1rem] overflow-auto p-[1rem]'
+                style={{ height: 'calc(100% - 9rem)' }}
+              >
+                {messageListController.state.objs.map((message) => (
+                  <div className='rounded-[2rem] bg-gradient-to-br from-slate-900 to-slate-800 p-[1rem] text-white'>
+                    {message.message}
+                  </div>
+                ))}
+              </div>
+              <GlassWindowFrame className='h-[5rem] w-full flex-shrink-0'>
+                <GlassWindowContents className='p-[1rem]'>
+                  <GlassWindowFrame
+                    className='h-full w-full flex-shrink-0'
+                    roundedFx={roundedFx['rounded-full']}
+                  >
+                    <GlassWindowPane glassFx={glassFx['glass-10']} />
+                  </GlassWindowFrame>
+                </GlassWindowContents>
+                <GlassWindowPane glassFx={glassFx['glass-10']} />
+              </GlassWindowFrame>
+            </GlassWindowContents>
+          </GlassWindowFrame>
+        </div>
+      )}
     </SpaceMapSidebarContainer>
   );
 }
