3a28a6bce592ada62ca2ee4ff2baf5b7ab0419a9
Jason Yu <jasonyu0100@gmail.com>
2024-09-09 14:29:28 +1000

    addressing build error

--- app/(core)/(dashboard)/network/(pages)/creative/view/main/table/results/row/number/play/main.tsx
+++ app/(core)/(dashboard)/network/(pages)/creative/view/main/table/results/row/number/play/main.tsx
@@ -1,4 +1,4 @@
-import { userProfileMap } from '@/(core)/(profile)/user/[id]/map';
+import { userProfileMap } from '@/(core)/(dashboard)/user/[id]/map';
 import { ContextForUserObj } from '@/(server)/model/user/main';
 import { AstralPlayIcon } from '@/icons/play/main';
 import { useContext } from 'react';
