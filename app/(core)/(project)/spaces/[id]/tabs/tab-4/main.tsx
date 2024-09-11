f75fc4157842d0c556352a44c9ab28f75add7b83
Jason Yu <jasonyu0100@gmail.com>
2024-09-11 19:00:50 +1000

    adding spotlight sync

--- app/(core)/(project)/spaces/[id]/tabs/tab-4/main.tsx
+++ app/(core)/(project)/spaces/[id]/tabs/tab-4/main.tsx
@@ -4,6 +4,7 @@ import { Tab } from '@/(core)/(dashboard)/common//tabs/tabs-row/tab/main';
 import { TabPurpleShard } from '@/(core)/(dashboard)/common//tabs/tabs-row/tab/shard/purple/main';
 import { TabText } from '@/(core)/(dashboard)/common//tabs/tabs-row/tab/text/main';
 import { ContextForSpaceChapterList } from '@/(server)/controller/space/chapter/list';
+import { ContextForChapterSpotlightListFromChapter } from '@/(server)/controller/space/chapter/spotlight/list-from-chapter';
 import { useContext } from 'react';
 import { CreativeSpacesContext } from '../../layout';
 import { spaceMap } from '../../map';
@@ -11,16 +12,20 @@ import { SpaceTabStage } from '../main';
 
 export function SpacesTabFlight({ tab }: { tab?: string }) {
   const chapterListController = useContext(ContextForSpaceChapterList);
+  const spotlightListController = useContext(
+    ContextForChapterSpotlightListFromChapter,
+  );
   const spaceContext = useContext(CreativeSpacesContext);
   const tabText = SpaceTabStage.Flight;
   const active = tab === tabText;
 
   const spaceId = spaceContext?.spaceId;
   const chapterId = chapterListController?.state?.objId;
+  const spotlightId = spotlightListController?.state?.objId;
 
   // Construct the URL only if spaceId is available
   const url = spaceId
-    ? `${spaceMap.space.id.flight.link(spaceId)}?chapter=${chapterId ?? ''}`
+    ? `${spaceMap.space.id.flight.link(spaceId)}?chapter=${chapterId ?? ''}&spotlight=${spotlightId ?? ''}`
     : '#'; // Fallback URL if spaceId is undefined
 
   return (
