364dfc5366356468cbc93ff8195c068d0190267c
Jason Yu <jasonyu0100@gmail.com>
2024-10-09 15:57:02 +1100

    updating chapters add

--- app/(core)/(project)/spaces/[id]/(pages)/space/view/sidebar/chapters/list/add/main.tsx
+++ app/(core)/(project)/spaces/[id]/(pages)/space/view/sidebar/chapters/list/add/main.tsx
@@ -1,3 +1,4 @@
+import { AstralAddIcon } from '@/icons/add/main';
 import { useContext } from 'react';
 import { ContextForSpacesSpaceModals } from '../../../../../modal/controller/main';
 
@@ -5,15 +6,15 @@ export function SpacesSpaceSidebarChaptersAdd() {
   const modalController = useContext(ContextForSpacesSpaceModals);
 
   return (
-    <button
-      className='mt-[1rem] flex w-full'
-      onClick={() => {
-        modalController.addChapterController.open();
-      }}
-    >
-      <p className='animate-pulse-slow font-extraBold text-xl text-slate-300'>
-        Add Chapter
-      </p>
-    </button>
+    <div className='flex w-full flex-row items-center justify-center'>
+      <button
+        className='flex h-[3rem] w-[3rem] items-center justify-center rounded-full bg-blue-500'
+        onClick={() => {
+          modalController.addChapterController.open();
+        }}
+      >
+        <AstralAddIcon />
+      </button>
+    </div>
   );
 }
