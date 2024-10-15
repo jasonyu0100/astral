88999d451f4ba6d80c2dd8b81f5290f8c6365fe3
Jason Yu <jasonyu0100@gmail.com>
2024-10-16 07:27:11 +1100

    fixing odd login issue

--- app/(portal)/common/container/main.tsx
+++ app/(portal)/common/container/main.tsx
@@ -1,6 +1,6 @@
 export function PortalContainer({ children }: { children?: React.ReactNode }) {
   return (
-    <div className='aspect-[13/16] h-full flex-shrink-0 bg-white shadow-2xl'>
+    <div className='aspect-[13/16] w-1/2 flex-shrink-0 bg-white shadow-2xl'>
       {children}
     </div>
   );
