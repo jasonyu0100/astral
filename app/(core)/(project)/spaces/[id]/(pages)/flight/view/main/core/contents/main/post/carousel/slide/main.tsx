7d194fdb0f02bc59a5dba8d06614b0fc91a5764b
Jason Yu <jasonyu0100@gmail.com>
2024-10-21 15:11:36 +1100

    using element attachment

--- app/(core)/(project)/spaces/[id]/(pages)/flight/view/main/core/contents/main/post/carousel/slide/main.tsx
+++ app/(core)/(project)/spaces/[id]/(pages)/flight/view/main/core/contents/main/post/carousel/slide/main.tsx
@@ -1,21 +1,23 @@
 import { ContextForPostAttachmentListFromPost } from '@/(server)/controller/post/attachment/list-from-post';
+import {
+  ContextForPostAttachmentObj,
+  examplePostAttachment,
+} from '@/(server)/model/post/attachment/main';
+import { ElementAttachment } from '@/ui/element/attachment/main';
 import { useContext } from 'react';
 
 export function SpacesFlightCarouselSlide({ index }: { index: number }) {
   const attachmentListController = useContext(
     ContextForPostAttachmentListFromPost,
   );
-  const images = attachmentListController.state.objs.map(
-    (obj) => obj.fileElem?.src || '',
-  );
+  const attachment =
+    attachmentListController.state.objs.at(index) || examplePostAttachment;
 
   return (
     <div className='aspect-video w-full overflow-hidden rounded-[1rem] bg-black'>
-      <img
-        src={images[index]}
-        alt={`Slide ${index}`}
-        className='h-full w-full object-contain'
-      />
+      <ContextForPostAttachmentObj.Provider value={attachment}>
+        <ElementAttachment />
+      </ContextForPostAttachmentObj.Provider>
     </div>
   );
 }
