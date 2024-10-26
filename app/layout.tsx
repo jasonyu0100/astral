06fe93a1fe94136a9a93e4768a10f09bbb97eac7
Jason Yu <jasonyu0100@gmail.com>
2024-10-26 11:58:30 +1100

    using dangerously set

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,32 +8,40 @@ export default function Layout({ children }: { children?: React.ReactNode }) {
 
   return (
     <html lang='en'>
-      <title>Astral</title>
       <head>
+        <title>Astral</title>
         <meta name='referrer' content='origin' />
-      </head>
-      <body className={ctwn('bg-slate-950', [...fontVariables])}>
-        <Analytics />
-        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-16754167285">
-        </script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'AW-16754167285');
-        </script>
-        <script>
-            (function(h,o,t,j,a,r){
+        <script
+          async
+          src='https://www.googletagmanager.com/gtag/js?id=AW-16754167285'
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-16754167285');
+            `,
+          }}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(h,o,t,j,a,r){
                 h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
                 h._hjSettings={hjid:5185395,hjsv:6};
                 a=o.getElementsByTagName('head')[0];
                 r=o.createElement('script');r.async=1;
                 r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
                 a.appendChild(r);
-            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-        </script>
-        
+              })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+            `,
+          }}
+        />
+      </head>
+      <body className={ctwn('bg-slate-950', ...fontVariables)}>
+        <Analytics />
         <main>{!isMaintenanceMode && <>{children}</>}</main>
       </body>
     </html>
