8f5ab9db29fae299eae8866d5c0bd239a9985dc2
Jason Yu <jasonyu0100@gmail.com>
2024-11-01 17:14:08 +1100

    fixing build bug

--- app/(core)/(general)/layout.tsx
+++ app/(core)/(general)/layout.tsx
@@ -3,16 +3,9 @@ import { DashboardBody } from '@/(core)/(dashboard)/common/container/body/main';
 import { DashboardContainer } from '@/(core)/(dashboard)/common/container/main';
 import { CommonSidebar } from '@/(core)/common/(sidebar)/main';
 import { CommonTopbar } from '@/(core)/common/(topbar)/main';
-import { CentralWindowContainer } from '../common/central/main';
+import { CentralWindowContainer } from './common/central/main';
 
-export default function Layout({
-  children,
-  params,
-}: {
-  children?: React.ReactNode;
-
-  params: { id: string };
-}) {
+export default function Layout({ children }: { children?: React.ReactNode }) {
   return (
     <>
       <CommonTopbar />
