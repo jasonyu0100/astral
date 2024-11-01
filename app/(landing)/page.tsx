94facbf20f3208739311c01c0bfe0f6c96df58c5
Jason Yu <jasonyu0100@gmail.com>
2024-11-01 16:48:13 +1100

    change

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -129,7 +129,7 @@ const CallToAction = () => {
         loop
       /> */}
       <iframe
-        src='https://www.youtube.com/embed/EiMj6UVBT_c'
+        src='https://www.youtube.com/embed/3m51tfnCrWk'
         className='aspect-video w-[3/4] flex-shrink-0 rounded-[1rem] shadow-glow sm:w-3/4'
         allow='autoplay; encrypted-media'
         allowFullScreen
