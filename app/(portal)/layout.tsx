6b52b73cee928217fdeae49668fb3c4eb71a40a2
Jason Yu <jasonyu0100@gmail.com>
2024-11-04 17:59:31 +1100

    adding suspsense

--- app/(portal)/layout.tsx
+++ app/(portal)/layout.tsx
@@ -1,5 +1,10 @@
+import { Suspense } from 'react';
 import { PortalBackground } from './common/background/main';
 
 export default function Layout({ children }: { children?: React.ReactNode }) {
-  return <PortalBackground>{children}</PortalBackground>;
+  return (
+    <Suspense fallback={<PortalBackground />}>
+      <PortalBackground>{children}</PortalBackground>
+    </Suspense>
+  );
 }
