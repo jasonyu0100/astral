30cc4207a5ef3faf6b76dcb2606b3a6a58dc0d32
Jason Yu <jasonyu0100@gmail.com>
2024-11-05 18:27:32 +1100

    hidding add member from viewers

--- app/(core)/(project)/spaces/sidebar/members/main.tsx
+++ app/(core)/(project)/spaces/sidebar/members/main.tsx
@@ -1,15 +1,21 @@
 import { ContextForSpaceMemberList } from '@/architecture/controller/space/member/list';
 import { ContextForSpaceMemberObj } from '@/architecture/model/space/member/main';
+import {
+  ContextForUserPageRole,
+  UserPageRole,
+} from '@/architecture/model/user/main';
 import { useContext } from 'react';
 import { SpaceSidebarMembersListAdd } from './add/main';
 import { SpaceSidebarMembersListMember } from './member/main';
 
 export function SpaceSidebarMembersList() {
   const spaceMemberListController = useContext(ContextForSpaceMemberList);
+  const userRole = useContext(ContextForUserPageRole);
 
   return (
     <div className='flex h-full w-full flex-col items-center space-y-[2rem] overflow-auto rounded-full'>
-      <SpaceSidebarMembersListAdd />
+      {userRole === UserPageRole.OWNER ||
+        (userRole === UserPageRole.MEMBER && <SpaceSidebarMembersListAdd />)}
       {spaceMemberListController.state.objs.map((obj) => (
         <ContextForSpaceMemberObj.Provider value={obj}>
           <SpaceSidebarMembersListMember />
