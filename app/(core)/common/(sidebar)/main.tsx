b7202e5fa11404bee4a18733b740bc5924f9b9dc
Jason Yu <jasonyu0100@gmail.com>
2024-11-06 22:58:21 +1100

    hiding feed

--- app/(core)/common/(sidebar)/main.tsx
+++ app/(core)/common/(sidebar)/main.tsx
@@ -7,7 +7,6 @@ import { useGlobalUser } from '@/logic/store/user/main';
 import { createContext } from 'react';
 import { CommonSidebarContainer } from './common/container/default/main';
 import { CommonSidebarIndicators } from './common/indicators/main';
-import { CommonSidebarUpdatesIndicator } from './common/indicators/variants/feed/main';
 import { CommonSidebarJournalIndicator } from './common/indicators/variants/journal/main';
 import { CommonSidebarNetworkIndicator } from './common/indicators/variants/network/main';
 import { CommonSidebarSpaceIndicator } from './common/indicators/variants/studio/main';
@@ -53,7 +52,7 @@ export function CommonSidebar({
               <HorizontalDivider className='my-[1rem] mb-[2rem]' />
               <CommonSidebarIndicators>
                 <CommonSidebarSpaceIndicator />
-                <CommonSidebarUpdatesIndicator />
+                {/* <CommonSidebarUpdatesIndicator /> */}
                 <CommonSidebarJournalIndicator />
                 <CommonSidebarNetworkIndicator />
                 <CommonSidebarVaultIndicator />
