78befe12a6d754c5dc6835c5c487e0cae2b3da4b
Jason Yu <jasonyu0100@gmail.com>
2024-11-12 12:37:55 +1100

    updating astral

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -132,7 +132,7 @@ const CallToAction = () => {
         loop
       /> */}
       <iframe
-        src='https://www.youtube.com/embed/3m51tfnCrWk'
+        src='https://www.youtube.com/embed/wh7nnuBGmFQ'
         className='aspect-video w-[3/4] flex-shrink-0 rounded-[1rem] shadow-glow sm:w-3/4'
         allow='autoplay; encrypted-media'
         allowFullScreen
